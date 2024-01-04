e5b820a40838d8e84d26a3fa099da46b9673e799
nhanluongoe <nhanlt18@gmail.com>
2024-01-04 22:28:47 +0700

    feat: add animation for icon

--- src/components/toast-icon.tsx
+++ src/components/toast-icon.tsx
@@ -1,26 +1,50 @@
+import { keyframes, setup, styled } from 'goober';
+import React from 'react';
 import { Toast } from '../core/types';
+import { Check, Close } from './icons';
+
+setup(React.createElement);
 
 interface ToastIconProps {
   icon?: Toast['icon'];
   type: Toast['type'];
 }
 
+const enter = keyframes`
+from {
+  transform: scale(0.6);
+  opacity: 0.4;
+}
+to {
+  transform: scale(1);
+  opacity: 1;
+}`;
+
+export const AnimatedIconWrapper = styled('div')`
+  position: relative;
+  transform: scale(0.6);
+  opacity: 0.4;
+  min-width: 20px;
+  animation: ${enter} 0.3s 0.12s cubic-bezier(0.175, 0.885, 0.32, 1.275)
+    forwards;
+`;
+
 export default function ToastIcon(props: ToastIconProps) {
   const { icon, type } = props;
 
   if (icon !== undefined) {
-    return icon;
+    return <AnimatedIconWrapper>{icon}</AnimatedIconWrapper>;
   }
 
   const renderIcon = () => {
     if (type === 'success') {
-      return '✅';
+      return <Check />;
     }
     if (type === 'error') {
-      return '❌';
+      return <Close />;
     }
     return null;
   };
 
-  return <div>{renderIcon()}</div>;
+  return <AnimatedIconWrapper>{renderIcon()}</AnimatedIconWrapper>;
 }
