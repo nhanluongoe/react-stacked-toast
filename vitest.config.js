1e4bf5d812b24d6793de14517b1d0d0337575a1d
nhanluongoe <nhanlt18@gmail.com>
2024-01-11 15:20:53 +0700

    test: update setup file path

--- vitest.config.js
+++ vitest.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './tests/setup.js',
+    setupFiles: './tests/setup.ts',
     passWithNoTests: true,
   },
 });
